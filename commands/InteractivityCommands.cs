26db341a28666b32b881a31e1a6b609f905b6921
Ventus <aosica789@gmail.com>
2025-05-05 00:05:37 +0200

    Changed from CollectReactionAsync to GetReactionsAsync to count the number of reactions on message

--- commands/InteractivityCommands.cs
+++ commands/InteractivityCommands.cs
@@ -54,19 +54,13 @@ namespace VBotDiscord.commands {
                 await sentPoll.CreateReactionAsync(emoji);
             }
 
-            var pollTime = TimeSpan.FromSeconds(10);
-            var totalReactions = await interactivity.CollectReactionsAsync(sentPoll, pollTime);
-            int count1 = 0;
-            int count2 = 0;
-            int count3 = 0;
-            int count4 = 0;
+            var timePoll = TimeSpan.FromSeconds(10);
+            await Task.Delay(timePoll);
 
-            foreach (var emoji in totalReactions) {
-                if (emoji.Emoji == emojiOptions[0]) count1++;
-                if (emoji.Emoji == emojiOptions[1]) count2++;
-                if (emoji.Emoji == emojiOptions[2]) count3++;
-                if (emoji.Emoji == emojiOptions[3]) count4++;
-            }
+            int count1 = (await sentPoll.GetReactionsAsync(emojiOptions[0])).Count - 1;
+            int count2 = (await sentPoll.GetReactionsAsync(emojiOptions[1])).Count - 1;
+            int count3 = (await sentPoll.GetReactionsAsync(emojiOptions[2])).Count - 1;
+            int count4 = (await sentPoll.GetReactionsAsync(emojiOptions[3])).Count - 1;
 
             int totalVotes = count1 + count2 + count3 + count4;
             string resultsDescription = $"{emojiOptions[0]}: {count1} votes! \n" +
